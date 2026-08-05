fa572233afd5b36a39991922c4ca057d41b95de0
DiogoKovalek <diogokovalek@gmail.com>
2026-08-05 19:13:24 -0300

    feat(Movement player): player move at up, right, down and left

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,14 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    void Start()
-    {
-        
+public class Player : MonoBehaviour {
+    #region Movement
+    [SerializeField] private float distPoints = 1; // Distancia entre cada centro do quadrado para o player ficar no centro
+    [SerializeField] private float speedMovement = 8;
+    private bool isMoving = false;
+    private Vector2 targetMove;
+    #endregion
+
+    void Start() {
+
     }
-    void Update()
-    {
-        Debug.Log(InputManager.inputManager.GetMoveDirection());
+    void Update() {
+        //Movement
+        byte direction = InputManager.inputManager.GetMoveDirection();
+
+        if (direction != 0 && !isMoving) {
+            defineTargetForMove(direction);
+        }
+        if (isMoving) {
+            movement();
+        }
+    }
+
+    private void defineTargetForMove(byte direction) {
+        switch (direction) {
+            case 1: // UP
+                targetMove = (Vector2)transform.position + new Vector2(0, distPoints);
+                break;
+            case 2: // RIGHT
+                targetMove = (Vector2)transform.position + new Vector2(distPoints, 0);
+                break;
+            case 3: // DOWN
+                targetMove = (Vector2)transform.position + new Vector2(0, -distPoints);
+                break;
+            case 4: // LEFT
+                targetMove = (Vector2)transform.position + new Vector2(-distPoints, 0);
+                break;
+            default: // NONE
+                return;
+        }
+        isMoving = true;
+    }
+    private void movement() {
+        transform.position = Vector2.MoveTowards(transform.position, targetMove, speedMovement*Time.deltaTime);
+        if((Vector2) transform.position == targetMove) {
+            isMoving = false;
+        }
     }
 }
