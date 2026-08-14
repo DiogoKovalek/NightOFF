26b38c9076764471c87a01c59557c2be6ec21a2c
Diogo Kovalek <diogokovalek@gmail.com>
2026-08-14 10:35:13 -0300

    feat(Pause and Continue)

---
+++ Assets/Scripts/UI/UIControler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControler : MonoBehaviour {
+    //[Header("Buttons")]
+    //[Header("ShiftCounter")]
+    [Header("PauseMenu")]
+    [SerializeField] private GameObject pauseMenu;
+
+
+    public void OnPauseGame() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("Pause");
+    }
+    public void OnContinueGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Debug.Log("Continue");
+    }
+    public void OnRestartGame() {
+        Debug.Log("Restart");
+    }
+    public void OnExitGame() {
+        Debug.Log("Exit");
+    }
+}
