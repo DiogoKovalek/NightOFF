fe71ed61357887da91bf38c6e920889408d2224a
DiogoKovalek <diogokovalek@gmail.com>
2026-08-19 13:26:08 -0300

    feat(teleport working)

--- Assets/Scripts/Devices/Teleporter.cs
+++ Assets/Scripts/Devices/Teleporter.cs
@@ -27,54 +27,29 @@ public class Teleporter : Device {
         if (collision.CompareTag("Player")) {
             if (!inAreaOfTeleport) {
                 inAreaOfTeleport = true;
-                isTeleported = false;
                 StartCoroutine(teleportingProcess(collision));
+            }else if (inAreaOfTeleport && isInTeleportProcessing) {
+                isTeleported = true;
             }
         }
     }
     void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            if (isTeleported) {
+            if (inAreaOfTeleport && !isInTeleportProcessing) {
                 inAreaOfTeleport = false;
                 isTeleported = false;
             }
         }
     }
     
-
-    /*
-    void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log($"{gameObject.name}: Entrou no Teletransportador");
-        if (collision.CompareTag("Player") && !inAreaOfTeleport && !isInTeleportProcessing && coroutineTeleport == null) {
-            // Indica que entrou em area de teleport
-            // Assim nao vai teleportar infinitamente
-            Debug.Log($"{gameObject.name}: Iniciou o processo de Teleransportar");
-            inAreaOfTeleport = true;
-            destinationTeleport.SetInAreaOfTeleport(true);
-            coroutineTeleport = StartCoroutine(teleportingProcess(collision));
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D collision) {
-        Debug.Log($"{gameObject.name}: Saiu do Teletransportador");
-        if(collision.CompareTag("Player") && inAreaOfTeleport && !isInTeleportProcessing && coroutineTeleport == null) {
-            // Indica que acabou de sair da area de teleport
-            // depois que foi teletransportado
-            Debug.Log($"{gameObject.name}: Encerrou o processo de Teletransportar");
-            inAreaOfTeleport = false;
-            destinationTeleport.SetInAreaOfTeleport(false);
-        }
-    }
-    */
     private IEnumerator teleportingProcess(Collider2D collision) {
         Player player = collision.gameObject.GetComponentInParent<Player>();
         if(player == null) yield break;
-        if(isInTeleportProcessing) yield break;
+        if(isTeleported) yield break;
         // Seta atributos de controle
         isInTeleportProcessing = true;
         //destinationTeleport.SetIsInTeleportProcessing(true);
         
-
         while (player.GetIsMoving()) { // espera até o player para de se mover
             yield return null;
         }
@@ -82,14 +57,14 @@ public class Teleporter : Device {
         // Executa animacao de teleport
 
         // Teleporta
+        
         collision.transform.parent.position = GetPositionDestination();
-        yield return new WaitForSeconds(1f); // Tentart depois um yield return null
+        yield return new WaitUntil(() => isTeleported); // Tentart depois um yield return null
 
         // Executa animacao de finalizar teleport
 
         // Seta atributos que indicam finalizacao de teleport
         isInTeleportProcessing = false;
-        isTeleported = true;
         //destinationTeleport.SetIsInTeleportProcessing(false);
 
         //ADENDO -> depois deve travar a possibilidade do player se movimentar
