b62d3be607699416a40241708021b1d15289054b
Diogo Kovalek <diogokovalek@gmail.com>
2026-09-03 11:15:27 -0300

    feat(Generate Cloud)

--- Assets/Scripts/Clouds/Cloud.cs
+++ Assets/Scripts/Clouds/Cloud.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cloud : MonoBehaviour
-{
-    
+public class Cloud : MonoBehaviour, IMovable {
+    private float speed;
+    private Vector2 direction;
+    private bool isFreeForMove = false;
+
+    void Update() {
+        if (isFreeForMove) {
+            transform.position = (Vector2) transform.position + speed * direction * Time.deltaTime;
+        }
+    }
+
+    public void Movement(bool isMovement) {
+        isFreeForMove = isMovement;
+    }
+    public void SetDirection(Vector2 value) {
+        direction = value;
+    }
+    public void SetSpeed(float value) {
+        speed = value;
+    }
 }
--- Assets/Scripts/Clouds/CloudControler.cs
+++ Assets/Scripts/Clouds/CloudControler.cs
@@ -2,7 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudControler : MonoBehaviour
-{
-    
+public class CloudControler : MonoBehaviour {
+    [SerializeField] private GameObject prefabCloud;
+
+    [Header("Area Spawn")]
+    [SerializeField] private Vector2 spawnAreaDownLeft = Vector2.zero;
+    [SerializeField] private Vector2 spawnAreaUpRight = Vector2.zero;
+
+    [Header("Atributes Spawn")]
+    // Len
+    [SerializeField] private int minQuantPrefab = 2;
+    [SerializeField] private int maxQuantPrefab = 6;
+    // speed spawn
+    [SerializeField] private float minSpeedSpawn = 1;
+    [SerializeField] private float maxSpeedSpawn = 1.5f;
+
+    [Header("Atributes Prefab")]
+    // speed prefab
+    [SerializeField] private float minSpeedPrefab = 3;
+    [SerializeField] private float maxSpeedPrefab = 10;
+    // size prefab
+    [SerializeField] private float minScalePrefab = 0.8f;
+    [SerializeField] private float maxScalePrefab = 1.3f;
+    // alpha prefab
+    [SerializeField] private byte minAlphaPrefab = 230;
+    [SerializeField] private byte maxAlphaPrefab = 255;
+
+    void Start() {
+        spawnPrefab<Cloud>(Vector2.zero, Vector2.left, 3, 2, 200/255f);
+    }
+
+    private void spawnPrefab<T>(Vector2 position, Vector2 direction, float speed, float scale, float alpha)
+    where T : IMovable{
+        GameObject obj = Instantiate(prefabCloud, position, prefabCloud.transform.rotation, this.transform);
+        obj.transform.localScale = obj.transform.localScale * scale;
+        SpriteRenderer sprRen = obj.GetComponent<SpriteRenderer>();
+        if (sprRen != null) {
+            Color c = sprRen.color;
+            c.a = alpha;
+            sprRen.color = c;
+        }
+        T scr = obj.GetComponent<T>();
+        scr?.SetSpeed(speed);
+        scr?.SetDirection(direction);
+        scr?.Movement(true);
+    }
 }
