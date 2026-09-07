a99dfb14ca84404cdc42db2a72adb9bcba301843
Diogo Kovalek <diogokovalek@gmail.com>
2026-09-07 13:41:31 -0300

    feat(luzes list)

--- Assets/Scripts/Questionary/ControlerQuestionary.cs
+++ Assets/Scripts/Questionary/ControlerQuestionary.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 
 public class ControlerQuestionary : MonoBehaviour
 {
+    [Header("Caminho das perguntas")]
     [Tooltip("nomeArquivo deve estar dentro de Resource, Sem o .json")]
     [SerializeField] private String nomeArquivoJSON;
-    void Start(){
+
+    [Header("Placas")]
+    [SerializeField] private GameObject PlacaPergunta;
+    [SerializeField] private GameObject[] PlacaRespostas;
+    private LuzPlaca[] listLuzesPlacaPergunta;
+    private LuzPlaca[][] listLuzesPlaca;
+
+    void Awake(){
+        if(PlacaPergunta != null) listLuzesPlacaPergunta = PlacaPergunta.GetComponentsInChildren<LuzPlaca>();
+
+        listLuzesPlaca = new LuzPlaca[PlacaRespostas.Length][];
+        for(int i = 0; i < PlacaRespostas.Length; i++) {
+            if(PlacaRespostas[i] == null) continue;
+            listLuzesPlaca[i] = PlacaRespostas[i].GetComponentsInChildren<LuzPlaca>();
+        }
         CarregarJSON(nomeArquivoJSON);
+    }
+
+    void Start() {
+        foreach(var luz in listLuzesPlaca[1])luz.SwitchColor(COR_LUZ.AMARELO);
+        foreach(var luz in listLuzesPlaca[2])luz.SwitchColor(COR_LUZ.VERDE);
+        foreach(var luz in listLuzesPlaca[3])luz.SwitchColor(COR_LUZ.VERMELHO);
+        AscenderTodasAsLuzes(listLuzesPlacaPergunta);
+        foreach(var listLuz in listLuzesPlaca)AscenderTodasAsLuzes(listLuz);
     }
 
     private void CarregarJSON(String nomeArquivoJSON) {
         if(ListQuestions.listQuestion != null) return;
 
-        Debug.Log("Aqui");
         TextAsset arquivo = Resources.Load<TextAsset>(nomeArquivoJSON);
         if(arquivo != null) {
             QuestionWrapper dados = JsonUtility.FromJson<QuestionWrapper>(arquivo.text);
@@ -24,4 +46,12 @@ public class ControlerQuestionary : MonoBehaviour
             Debug.LogError($"Arquivo {nomeArquivoJSON} não foi encontrado");
         }
     }
+
+    #region Luz controler
+    private void AscenderTodasAsLuzes(LuzPlaca[] luzes) {
+        foreach(var luz in luzes) {
+            luz.EnableStaticColor(true);
+        }
+    }
+    #endregion
 }
