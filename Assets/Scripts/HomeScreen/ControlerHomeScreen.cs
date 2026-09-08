0fa779e77233d9cf752c53e087a22e3f5d9fb0ea
Diogo Kovalek <diogokovalek@gmail.com>
2026-09-08 14:42:18 -0300

    feat(Day night system in home menu)

--- Assets/Scripts/HomeScreen/ControlerHomeScreen.cs
+++ Assets/Scripts/HomeScreen/ControlerHomeScreen.cs
@@ -22,31 +22,39 @@ public class ControlerHomeScreen : MonoBehaviour {
     private PowerCableDevice[] listPowerCable;
     private SolarPanel[] listSolarPanel;
 
+    [Header("Background")]
+    [SerializeField] private bool initDay = true;
+    private bool isDay = true;
+    [SerializeField] private GameObject Background;
+    private const float timeSecondTradeDayNight = 0.15f;
+    private const float timeToSwitchTurn = 12f;
+
     #region EVENTS
     public delegate void DeviceSwitched(bool isDay);
     public event DeviceSwitched deviceSwitched;
     #endregion
     void Awake() {
+        isDay = initDay;
         conectEvents();
     }
     private void conectEvents() {
-        if(devices != null) listDevices = devices?.GetComponentsInChildren<Device>();
-        if(powerCables != null) listPowerCable = powerCables?.GetComponentsInChildren<PowerCableDevice>();
-        if(solarPanels != null) listSolarPanel = solarPanels?.GetComponentsInChildren<SolarPanel>();
+        if (devices != null) listDevices = devices?.GetComponentsInChildren<Device>();
+        if (powerCables != null) listPowerCable = powerCables?.GetComponentsInChildren<PowerCableDevice>();
+        if (solarPanels != null) listSolarPanel = solarPanels?.GetComponentsInChildren<SolarPanel>();
 
-        if(listDevices != null){
-            foreach(var dev in listDevices){
+        if (listDevices != null) {
+            foreach (var dev in listDevices) {
                 deviceSwitched += dev.OnDeviceSwitch;
             }
         }
-        if(listPowerCable != null){
-            foreach(var pow in listPowerCable) {
+        if (listPowerCable != null) {
+            foreach (var pow in listPowerCable) {
                 deviceSwitched += pow.OnDeviceSwitch;
             }
         }
-        if(listDevices != null) {
+        if (listDevices != null) {
             foreach (var sol in listSolarPanel) {
-               deviceSwitched += sol.OnDeviceSwitch; 
+                deviceSwitched += sol.OnDeviceSwitch;
             }
         }
     }
@@ -56,7 +64,8 @@ public class ControlerHomeScreen : MonoBehaviour {
         sliderBackground?.StartSlide();
         if (panelPressEnter != null) StartCoroutine(shiningPressEnter());
 
-        deviceSwitched?.Invoke(true);
+        deviceSwitched?.Invoke(isDay);
+        StartCoroutine(tradeDayNight());
     }
 
     void Update() {
@@ -72,6 +81,27 @@ public class ControlerHomeScreen : MonoBehaviour {
         while (true) {
             panelPressEnter.SetActive(!panelPressEnter.activeSelf);
             yield return new WaitForSeconds(delayShining);
+        }
+    }
+
+    private IEnumerator tradeDayNight() {
+        while (true) {
+            yield return new WaitForSeconds(timeToSwitchTurn);
+
+            Quaternion startRotation = Background.transform.rotation;
+            Quaternion endRotation = startRotation * Quaternion.Euler(0, 0, -180);
+            float countTime = 0;
+
+            while (countTime <= timeSecondTradeDayNight) {
+                countTime += Time.deltaTime;
+                float rot = countTime / timeSecondTradeDayNight;
+
+                Background.transform.rotation = Quaternion.Lerp(startRotation, endRotation, rot);
+                yield return null;
+            }
+            Background.transform.rotation = endRotation;
+            isDay = !isDay;
+            deviceSwitched?.Invoke(isDay);
         }
     }
 }
