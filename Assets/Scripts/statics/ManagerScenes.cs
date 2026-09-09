61a93a850b804fea13a05d96eacc04a7c68b71ce
DiogoKovalek <diogokovalek@gmail.com>
2026-09-09 14:59:35 -0300

    feat(Manager Scenes)

--- Assets/Scripts/statics/ManagerScenes.cs
+++ Assets/Scripts/statics/ManagerScenes.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public static class ManagerScenes
-{
-    private static string[] scenesLevel = {"TesteScene"};
+public static class ManagerScenes {
+    private static string[] scenesLevel = { "Level 1", "Level 2", "Level 3", "Level 4", "Level 5" };
     private static string sceneHomeScreen = "HomeScreen";
+    private static string miniGameScreen = "Questionary";
     private static int numLevel = 1; // 0 -> nao esta em nivel
+    private static bool inQuestionary = false;
 
     public static void NextLevel() {
         Time.timeScale = 1f;
-        numLevel++;
-        if(scenesLevel.Length < numLevel) numLevel = 1;
-        SceneManager.LoadScene(scenesLevel[numLevel - 1]);
+        if (!inQuestionary) {
+            inQuestionary = true;
+            SceneManager.LoadScene(miniGameScreen);
+        }
+        else {
+            inQuestionary = false;
+            numLevel++;
+            if (scenesLevel.Length < numLevel) ExitToHomeScreen();
+            SceneManager.LoadScene(scenesLevel[numLevel - 1]);
+        }
     }
     public static void RestartLevel() {
         Time.timeScale = 1f;
